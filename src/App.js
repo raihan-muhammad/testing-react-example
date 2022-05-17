894e9a36de875be9a68f6715f8fc0ae275a51f71
raihanmuhammad <michael.raihan@stockbit.com>
2022-05-17 19:29:20 +0700

    feat: add media handling and data visualization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import ListCar from "./Listcar/ListCar";
+import Dropzone from "react-dropzone-uploader";
+import "react-dropzone-uploader/dist/styles.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [files, setFiles] = useState();
+
+  const getUploadParams = ({ meta }) => {
+    setFiles(meta);
+    return { url: "https://httpbin.org/post" };
+  };
+
+  const hanldeChangeStatus = ({ meta, file }, status) => {
+    console.log(`Perubahaan terjadi ${status}`);
+    setFiles(meta);
+  };
+
+  const handleSubmit = (files, allFiles) => {
+    console.log(`Files => ${files} && all Files => ${allFiles}`);
+    allFiles.forEach((data) => data.remove());
+  };
+
+  useEffect(() => {}, [files]);
+
   return (
     <>
+      <Dropzone
+        getUploadParams={getUploadParams}
+        onChangeStatus={hanldeChangeStatus}
+        onSubmit={handleSubmit}
+        accept="image/*"
+      />
       <ListCar />
       <div className="App">
         <header className="App-header">
